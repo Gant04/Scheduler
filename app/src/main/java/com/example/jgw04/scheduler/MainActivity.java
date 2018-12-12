5fa9817919bbb17bf13a8f0b633cc4214b08a447
jgw04 <gjung10@illinois.edu>
2018-12-12 03:12:25 -0600

    adsf

--- app/src/main/java/com/example/jgw04/scheduler/MainActivity.java
+++ app/src/main/java/com/example/jgw04/scheduler/MainActivity.java
@@ -1,30 +1,92 @@
 package com.example.jgw04.scheduler;
-import android.app.Activity;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Calendar;
 
+import static com.example.jgw04.scheduler.R.id.notification_background;
+import static com.example.jgw04.scheduler.R.id.setButton;
 
-public class MainActivity extends Activity{
-    //get current time in milliseconds
-    long now = System.currentTimeMillis();
-    //save current time in date
-    Date date = new Date(now);
-    //set format to show time
-    SimpleDateFormat sdfNow = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+public class MainActivity extends AppCompatActivity{
 
-    String formatDate = sdfNow.format(date);
-
-    TextView dateNow;
+    AlarmManager alarm_manager;
+    TimePicker alarm_timepicker;
+    Context context;
+    PendingIntent pendingIntent;
 
     @Override
-    protected void Oncreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mainactivity);
+        setContentView(R.layout.activity_setalarm);
 
-        dateNow = (TextView) findViewById(R.id.dateNow);
-        dateNow.setText(formatDate);
+        this.context = this;
+
+        // 알람매니저 설정
+        alarm_manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        // 타임피커 설정
+        alarm_timepicker = findViewById(R.id.timePicker);
+
+        // Calendar 객체 생성
+        final Calendar calendar = Calendar.getInstance();
+
+        // 알람리시버 intent 생성
+        final Intent my_intent = new Intent(this.context, AlarmReceiver.class);
+
+        // 알람 시작 버튼
+        Button setAlarm = findViewById(R.id.setButton);
+        setAlarm.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+
+                // calendar에 시간 셋팅
+                calendar.set(Calendar.HOUR_OF_DAY, alarm_timepicker.getHour());
+                calendar.set(Calendar.MINUTE, alarm_timepicker.getMinute());
+
+                // 시간 가져옴
+                int hour = alarm_timepicker.getHour();
+                int minute = alarm_timepicker.getMinute();
+                Toast.makeText(MainActivity.this,"Alarm on " + hour + " : " + minute,Toast.LENGTH_SHORT).show();
+
+                // reveiver에 string 값 넘겨주기
+                my_intent.putExtra("state","alarm on");
+
+                pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, my_intent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+
+                // 알람셋팅
+                alarm_manager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                        pendingIntent);
+            }
+        });
+
+        // 알람 정지 버튼
+        Button alarm_off = findViewById(R.id.cancelButton);
+        alarm_off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Alarm ended",Toast.LENGTH_SHORT).show();
+                // 알람매니저 취소
+                alarm_manager.cancel(pendingIntent);
+
+                my_intent.putExtra("state","alarm off");
+
+                // 알람취소
+                sendBroadcast(my_intent);
+            }
+        });
     }
 }
